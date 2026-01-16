bc22b2d1ce0aa62117bb74534ce83c739e52fb55
Alex Ewerlöf <alex.ewerlof@gmail.com>
2026-01-16 15:58:23 +0100

    add the ui.css style to <render-markdown> for consistency

--- www/components/render-markdown.js
+++ www/components/render-markdown.js
@@ -1,4 +1,4 @@
-import { JJCC } from '../../lib/bundle.js'
+import { fetchCss, JJCC } from '../../lib/bundle.js'
 import markdownIt from 'https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/+esm'
 
 const md = markdownIt()
@@ -18,6 +18,7 @@ async function loadFile(filePath) {
 export class RenderMarkdown extends JJCC {
     static jj = {
         name: 'render-markdown',
+        styles: fetchCss(import.meta.resolve('../ui.css'))
     }
 
     static observedAttributes = ['file']
