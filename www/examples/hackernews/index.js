b9c05edc7f7bc2c79d7782ef0ee3e0cf2be5b044
Alex Ewerlöf <hanifbbz@gmail.com>
2026-03-30 09:28:43 +0000

    chore: simplify and improve the hackernews example

--- www/examples/hackernews/index.js
+++ www/examples/hackernews/index.js
@@ -3,19 +3,34 @@ import { stories } from './stories.js'
 
 const h = JJHE.tree
 
+function a(text, url = '#') {
+    return h('a', { href: url }, text)
+}
+
 JJD.from(document)
     .find('#stories', true)
     .addChildMap(stories, ({ title, id, host, points, age, comments }) =>
         h(
             'li',
             { class: 'hn__item' },
-            h('span', { class: 'hn__item-upvote' }, '▲'),
+            h('span', null, '▲'),
             h(
                 'div',
                 { class: 'hn__item-content' },
-                h('a', { href: '#' }, title),
+                a(title, '#'),
                 h('small', null, `(${host})`),
-                h('p', null, `${points} points by ${id} ${age} | hide | ${comments} comments`),
+                h(
+                    'p',
+                    null,
+                    `${points} points by `,
+                    a(id, '#'),
+                    ' ',
+                    a(age, '#'),
+                    ' | ',
+                    a('hide', '#'),
+                    ' | ',
+                    a(`${comments} comments`, '#'),
+                ),
             ),
         ),
     )
