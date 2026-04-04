784acf918714f601002b4cbc336329de3e76384f
Alex Ewerlöf <hanifbbz@gmail.com>
2026-04-04 19:49:51 +0000

    refactor: small improvement of the tutorial

--- www/tutorial/index.js
+++ www/tutorial/index.js
@@ -2,14 +2,13 @@ import { JJD } from '../../lib/bundle.js'
 import { CodeHighlight } from '../components/code-highlight.js'
 import { RenderMarkdown } from '../components/render-markdown.js'
 
-await CodeHighlight.defined
-await RenderMarkdown.defined
+await Promise.all([CodeHighlight.defined, RenderMarkdown.defined])
 
 const doc = JJD.from(document)
 const content = doc.find('#content', true)
 
 async function fetchFile(path) {
-    const url = new URL('./' + path, window.location.href)
+    const url = new URL(path, window.location.href)
     const response = await fetch(url)
     if (!response.ok) {
         throw new Error(`Failed to fetch ${url}: ${response.status} ${response.statusText}`)
